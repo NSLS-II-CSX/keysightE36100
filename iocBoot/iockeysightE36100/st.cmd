a1fcb1c7ebe9391c83a73e3f841eda77d9f2b703
Stuart B. Wilkins <swilkins@bnl.gov>
2017-06-30 23:44:31 -0400

    Built out st.cmd

--- iocBoot/iockeysightE36100/st.cmd
+++ iocBoot/iockeysightE36100/st.cmd
@@ -3,16 +3,44 @@
 ## You may have to change keysightE36100 to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase("../../dbd/keysightE36100.dbd",0,0)
 keysightE36100_registerRecordDeviceDriver(pdbbase) 
 
+epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST" , "NO")
+epicsEnvSet("EPICS_CA_ADDR_LIST"      , "10.23.0.255")
+epicsEnvSet("STREAM_PROTOCOL_PATH"    , "$(TOP)/protocols")
+
+drvAsynIPPortConfigure("KS1", "10.23.3.60:5025")
+asynSetTraceMask("KS1", 0, 0x9)
+asynSetTraceIOMask("KS1", 0, 0x2)
+
 ## Load record instances
-dbLoadRecords("../../db/keysightE36100.db","user=swilkins")
+#dbLoadRecords("db/keysightE36100.db","user=swilkins")
+
+asSetFilename("/epics/xf/23id/xf23id.acf")
+
+system("install -m 777 -d $(TOP)/as/save") 
+system("install -m 777 -d $(TOP)/as/req")
+
+set_savefile_path("${TOP}/as","/save")
+set_requestfile_path("${TOP}/as","/req")
+set_pass0_restoreFile("info_positions.sav")
+set_pass1_restoreFile("info_settings.sav")
 
 iocInit()
 
-## Start any sequence programs
-#seq snckeysightE36100,"user=swilkins"
+caPutLogInit("xf23id-ca.cs.nsls2.local:7004", 0)
+
+cd ${TOP}/as/req
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5 , "")
+create_monitor_set("info_settings.req", 15 , "")
+
+dbl > ${TOP}/records.dbl
+system("cp ${TOP}/records.dbl /cf-update/xf23id1-ioc2.es-keysightE36100.dbl")
+
---
+++ st.cmd
@@ -0,0 +1,2 @@
+#!/bin/bash
+(cd iocBoot/iockeysightE36100 && ./st.cmd)
